c3a6de59cc0e86e8dd5ab787b823d1b38b1b7872
Mathieu Servais <mercadis44@gmail.com>
2016-02-10 23:15:37 +0100

    Add CustomEvent on storageEvent (other tab)

--- src/app/message-storage.ts
+++ src/app/message-storage.ts
@@ -1,7 +1,27 @@
+import {EventEmitter} from "angular2/core";
+import {BrowserDomAdapter} from "angular2/platform/browser";
 import {Message} from "./message";
 
 export class MessageStorage {
   private static KEY_STORE: string = "messages";
+
+  public newMessageEvent: EventEmitter<Message>;
+
+  public constructor() {
+    this.newMessageEvent = new EventEmitter();
+    this.initSubscribStorageEvent();
+  }
+
+  private initSubscribStorageEvent() {
+    let dom = new BrowserDomAdapter();
+    let windowEvent = dom.getGlobalEventTarget("window");
+    windowEvent.addEventListener("storage", (event: StorageEvent) => {
+      if (event.key !== MessageStorage.KEY_STORE) return;
+      let messageList: Message[] = JSON.parse(event.newValue);
+      let lastMessage = messageList.pop();
+      this.newMessageEvent.emit(lastMessage);
+    }, false);
+  }
 
   public getList(): Message[] {
     return JSON.parse(localStorage.getItem(MessageStorage.KEY_STORE) || "[]");
