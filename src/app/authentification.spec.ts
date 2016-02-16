b7a98172cbc071087c4ab0c596076a3ca433dbf2
Mathieu Servais <mercadis44@gmail.com>
2016-02-16 22:24:35 +0100

    use rxjs for update nickname

--- src/app/authentification.spec.ts
+++ src/app/authentification.spec.ts
@@ -4,11 +4,17 @@ import {Authentification} from "./authentification";
 describe("Authentification", () => {
   it("should have default nickname", () => {
     let auth = new Authentification();
-    expect(auth.nickname.length).toBeGreaterThan(0);
+    auth.getNickname().subscribe(nickname => {
+      expect(nickname).toBe("anonymous");
+    });
   });
   it("should have nickname property set", () => {
     let auth = new Authentification();
-    auth.nickname = "mercadis";
-    expect(auth.nickname).toBe("mercadis");
+    let actual: string;
+    auth.getNickname().subscribe(nickname => {
+      actual = nickname;
+    });
+    auth.setNickname("mercadis");
+    expect(actual).toBe("mercadis");
   });
 });
--- src/app/authentification.ts
+++ src/app/authentification.ts
@@ -1,8 +1,15 @@
+import {Observable, Subject} from "rxjs/Rx";
 
 export class Authentification {
   private static DEFAULT_NICKNAME: string = "anonymous";
-  public nickname: string;
+  private subjectNickname: Subject<string>;
   constructor() {
-    this.nickname = Authentification.DEFAULT_NICKNAME;
+    this.subjectNickname = new Subject();
+  }
+  public getNickname(): Observable<string> {
+    return this.subjectNickname.merge(Observable.of(Authentification.DEFAULT_NICKNAME));
+  }
+  public setNickname(nickname: string) {
+    this.subjectNickname.next(nickname);
   }
 }
