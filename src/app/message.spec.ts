71e6ae992af6edda6dfe8b371b72c9bcc2293372
Mathieu Servais <mercadis44@gmail.com>
2016-02-16 22:26:25 +0100

    Clean

--- src/app/message.spec.ts
+++ src/app/message.spec.ts
@@ -2,17 +2,12 @@ import {describe, it, expect, beforeEach} from "angular2/testing";
 import {Message} from "./message";
 
 describe("Message", () => {
-  let message: Message;
-
-  beforeEach(() => {
-    message = new Message("nick", "body of message");
-  });
-
   it("should have a nickname", () => {
+    let message = new Message("nick", "body of message");
     expect(message.nickname).toBe("nick");
   });
-
   it("should have a body", () => {
+    let message = new Message("nick", "body of message");
     expect(message.body).toBe("body of message");
   });
 });
