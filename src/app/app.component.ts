fd4b3e3d68f063b6728cd650d42fb290a51eea4e
Mathieu Servais <mercadis44@gmail.com>
2016-02-17 20:55:26 +0100

    use header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,19 @@
 import {Component} from "angular2/core";
-import {MessageAddComponent} from "./message-add.component";
+import {HeaderComponent} from "./header.component";
 import {MessageListComponent} from "./message-list.component";
+import {MessageAddComponent} from "./message-add.component";
+
 
 @Component({
     selector: "app",
     template: `
-      <nav class="navbar navbar-fixed-top navbar-light bg-faded">
-        <a class="navbar-brand" href="#">Chat Client Standalone</a>
-        <ul class="nav navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" [href]="url" target="_blank">Add user</a>
-          </li>
-        </ul>
-      </nav>
+      <header></header>
       <div id="wrapper-static-bar">
         <message-list></message-list>
       </div>
       <message-add class="navbar-fixed-bottom"></message-add>
     `,
-    directives: [MessageAddComponent, MessageListComponent]
+    directives: [MessageAddComponent, MessageListComponent, HeaderComponent]
 })
 export class AppComponent {
   public url: string;
